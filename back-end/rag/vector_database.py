3c8d879382b724adf995346a616ca06e3347e272
emilyrhee <emilyrhee99@gmail.com>
2026-04-21 11:39:15 -0400

    feat: add vector database

---
+++ back-end/rag/vector_database.py
@@ -0,0 +1,14 @@
+from langchain_core.vectorstores import InMemoryVectorStore
+
+class Retriever:
+    def __init__(self, texts: list[str], embeddings_model, k: int = 5):
+        self.vector_store = InMemoryVectorStore.from_texts(
+            texts=texts, embedding=embeddings_model
+        )
+        self.retriever = self.vector_store.as_retriever(search_kwargs={"k": k})
+
+    def retrieve_chunks(self, query: str) -> list[str]:
+        return self.retriever.invoke(query)
+
+    def retrieve_with_scores(self, query: str, k: int = 5):
+        return self.vector_store.similarity_search_with_score(query, k=k)
