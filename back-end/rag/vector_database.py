1ffb8db1448a1ee424e6d88f1e091c2d65769ae2
emilyrhee <emilyrhee99@gmail.com>
2026-04-21 11:49:17 -0400

    refactor: renaming stuff

--- back-end/rag/vector_database.py
+++ back-end/rag/vector_database.py
@@ -1,14 +1,15 @@
 from langchain_core.vectorstores import InMemoryVectorStore
 
-class Retriever:
+
+class VectorDatabase:
     def __init__(self, texts: list[str], embeddings_model, k: int = 5):
         self.vector_store = InMemoryVectorStore.from_texts(
             texts=texts, embedding=embeddings_model
         )
         self.retriever = self.vector_store.as_retriever(search_kwargs={"k": k})
 
-    def retrieve_chunks(self, query: str) -> list[str]:
+    def get_chunks(self, query: str) -> list[str]:
         return self.retriever.invoke(query)
 
-    def retrieve_with_scores(self, query: str, k: int = 5):
+    def get_chunks_with_scores(self, query: str, k: int = 5):
         return self.vector_store.similarity_search_with_score(query, k=k)
